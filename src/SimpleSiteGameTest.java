fda6028da1636fa2c82fe14db501f80a43ffd49a
mpowerVS <153754901+mpowerVS@users.noreply.github.com>
2024-01-07 15:27:53 +0200

    Add files via upload

--- src/SimpleSiteGameTest.java
+++ src/SimpleSiteGameTest.java
@@ -1,15 +1,13 @@
-public class SimpleSiteGameTest {
-    public static void main(String[] args) {
-        SimpleSiteGame game = new SimpleSiteGame();
+/*public class SimpleSiteGameTest {
+        SimpleSite game1 = new SimpleSite();
 
         int[] locations = {2,3,4};
-        game.setLocationCells(locations);
+        game1.setLocationCells(locations);
 
         String userGuess = "2";
-        String result = game.check(userGuess);
+        String result = game1.check(userGuess);
         String testResult = "FAIL";
         if(result.equals("HIT"))
             testResult = "SUCCESS";
         System.out.println(testResult);
-    }
-}
+}*/
